138bfa690c920c7ee7b769857ea753ea336b2724
mohadmin25 <abuhamar008@gmail.com>
2022-08-29 14:24:45 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+// function superbowlWin(array){
+//     const result = array.find(function(){
+//         array.result === 'W'
+//     })
+//     return !! result ? array.year : undefined;
+// }
+function superbowlWin(record){
+    const result = record.find(rec => rec.result === 'W')
+    return !! result ? result.year : undefined;
+}
